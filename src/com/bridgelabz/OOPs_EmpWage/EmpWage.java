16f48b5d0c8ee61e15befb00cf3562d88475b141
PremKumbhar <kumbharprem7738@gmail.com>
2022-08-10 14:24:14 +0530

    [Prem]:Added employee wage problem using OOPs

---
+++ src/com/bridgelabz/OOPs_EmpWage/EmpWage.java
@@ -0,0 +1,47 @@
+package com.bridgelabz.OOPs_EmpWage;
+
+public class EmpWage 
+{
+	int wagePerHour = 20;
+	int fullTimeHour = 8;
+	int partTimeHour = 4;
+	int workingDayPerMonth = 20;
+	int totalWorkingHour = 100;
+	int totalEmpHour = 0;
+	int totalWorkingDays = 0;
+	
+	public void computeEmpWage() 
+	{
+		while (totalEmpHour < totalWorkingHour && totalWorkingDays < workingDayPerMonth) {
+			totalWorkingDays++;
+			int isPresent=(int ) Math.floor(Math.random()*10)%3;
+			switch (isPresent) {
+			case 0:
+				//System.out.println("Employee is present");
+				totalEmpHour += fullTimeHour;
+				break;
+			case 1:
+				//System.out.println("Employee is present as a part time");
+				totalEmpHour += partTimeHour;
+				break;
+			case 2:
+				//System.out.println("Employee is absent");
+				totalEmpHour += 0;
+				break;	
+			}
+		}
+		System.out.println("Monthly Wage : "+totalEmpHour*wagePerHour);
+	}
+
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Employee wage program");
+		
+		// Creating Object
+		EmpWage employeeWage = new EmpWage();
+		// Calling method
+		employeeWage.computeEmpWage();
+	}
+}
+
